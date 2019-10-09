3556a9cc5b1ee52730a4fada3a37dea4da85367e
TetaTeuta <teuta.stazic@gmail.com>
2019-10-10 11:36:42 +1300

    Update index.js

--- index.js
+++ index.js
@@ -278,6 +278,6 @@ app.delete('/users/:username', passport.authenticate('jwt', { session: false }),
 // listen for requests
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
+console.log("Listening on all ports");
 });
 
