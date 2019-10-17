cb3701aad54ee6f4786fc124540fa87ed9d6c929
TetaTeuta <teuta.stazic@gmail.com>
2019-10-17 19:59:50 +1300

    Update models.js

--- models.js
+++ models.js
@@ -2,45 +2,46 @@ const mongoose = require('mongoose');
 
 
 var movieSchema = mongoose.Schema({
-    Title : {type: String, required: true},
-    Description : {type: String, required: true},
-    Genre : {
-      Name : String,
-      Description : String
-    },
-    Director : {
-      Name : String,
-      Bio : String
-    },
-    Actors : [String],
-    ImagePath : String,
-    Featured : Boolean
-  });
-  
-  const bcrypt = require('bcrypt');
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
 
-  var userSchema = mongoose.Schema({
-    Username : {type: String, required: true},
-    Password : {type: String, required: true},
-    Email : {type: String, required: true},
-    Birthday : Date,
-    FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
-  });
-  
-  userSchema.statics.hashPassword = function(password) {
-    return bcrypt.hashSync(password, 10);
-  };
-  
-  userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password); };
+const bcrypt = require('bcrypt');
 
-  
-  
-  var Movie = mongoose.model('Movie', movieSchema);  //this creates db.movies somewhere else
-  var User = mongoose.model('User', userSchema);
-  
-  module.exports.Movie = Movie;    //this exports modules
-  module.exports.User = User;    //if specify user will create users
+var userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
+});
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
+
+
+var Movie = mongoose.model('Movie', movieSchema, 'Movies');  //this creates db.movies somewhere else
+var User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;    //this exports modules
+module.exports.User = User;    //if specify user will create users
 
 
 
