8bf7f8e2ae2053e7450fbfabb39c04b31a587e3d
TetaTeuta <teuta.stazic@gmail.com>
2019-10-29 21:22:02 +1300

    Update passport.js

--- passport.js
+++ passport.js
@@ -11,36 +11,36 @@ passport.use(new LocalStrategy(
   {
     usernameField: 'Username',
     passwordField: 'Password'
-  }, 
+  },
   (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username.'});
-    }
-    if (!user.validatePassword(password)) {
-      console.log('incorrect password');
-      return callback(null, false, {message: 'Incorrect password.'});
-    }
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+      if (!user) {
+        console.log('incorrect username');
+        return callback(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, { message: 'got this far' });
+      }
+      console.log('finished');
+      return callback(null, user);
+    });
+  }));
 
 passport.use(new JWTStrategy({
- jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
- secretOrKey: 'your_jwt_secret'
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
- return Users.findById(jwtPayload._id)
- .then((user) => {
-   return callback(null, user);
- })
- .catch((error) => {
-   return callback(error)
- });
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
 }));
