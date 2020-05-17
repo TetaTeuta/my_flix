c8cb3f6abcec909881c44a2b8ea05a8429931266
TetaTeuta <teuta.stazic@gmail.com>
2020-05-17 18:28:07 +0200

    Updating documentation

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -7,45 +7,53 @@ import './director-view.scss';
 import { Link } from "react-router-dom";
 import { MovieCard } from '../movie-card/movie-card';
 
+/**
+ * Director information view - renders the director name, bio, birth and a list of movies directed that
+ * are available in the database.
+ * @function DirectorView
+ * @param {string} props - director, movies
+ * @returns {DirectorView}
+ */
+
 export class DirectorView extends React.Component {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {};
-    }
+    this.state = {};
+  }
 
-    render() {
-        const { director } = this.props;
+  render() {
+    const { director } = this.props;
 
-        if (!director) return null;
+    if (!director) return null;
 
-        return (
-            <Card className="mb-1 mb-sm-2" style={{ maxHeight: '26rem' }}>
-                <Card.Body>
-                    <Card.Title className="director-name">{director.Name}</Card.Title>
-                    <Card.Text className="director-bio">
-                        Biography: <br />
-                        <br />
-                        {director.Bio}
-                        <br />
-                        <br />
-                        Birth Year:  {director.Birth}<br />
-                        Death year: {director.Death}
-                        <br />
-                        <br />
-                        <Link to={`/`}>
-                            <Button className="btn btn-dark" style={{ marginTop: '2rem' }}>Back</Button>
-                        </Link>
-                    </Card.Text>
+    return (
+      <Card className="mb-1 mb-sm-2" style={{ maxHeight: '26rem' }}>
+        <Card.Body>
+          <Card.Title className="director-name">{director.Name}</Card.Title>
+          <Card.Text className="director-bio">
+            Biography: <br />
+            <br />
+            {director.Bio}
+            <br />
+            <br />
+            Birth Year:  {director.Birth}<br />
+            Death year: {director.Death}
+            <br />
+            <br />
+            <Link to={`/`}>
+              <Button className="btn btn-dark" style={{ marginTop: '2rem' }}>Back</Button>
+            </Link>
+          </Card.Text>
 
-                </Card.Body>
-            </Card>
-        );
-    }
+        </Card.Body>
+      </Card>
+    );
+  }
 
 }
 
 DirectorView.propTypes = {
-    // will add later
+  // will add later
 };
